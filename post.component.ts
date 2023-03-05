32afc82305965533d20912dd950360bda05acebe
DINESH KUMAR <106733790+DINESHYNOT@users.noreply.github.com>
2023-03-05 11:16:45 +0530

    Add files via upload

---
+++ post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent {
+  title:string  = "Data binding String Interpolation";
+  //@Input() parent : string;
+
+}
